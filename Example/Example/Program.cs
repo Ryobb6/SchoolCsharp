c10dcaade661f6b19ec649d76e205d1db190e135
Ryobb6 <rryyoobb@gmail.com>
2021-08-24 11:33:40 +0900

    Project First Step
    ProjectのフォルダへGitignoreを置くこと

---
+++ Example/Example/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int money;
+            string name;
+
+                //変数に値を代入する
+
+                money = 5000;
+            name = "きたむら";
+
+
+            Console.WriteLine(money);
+            Console.WriteLine(name);
+   
+                
+               
+        }
+    }
+}
