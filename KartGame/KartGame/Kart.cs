dc1cf4a8191446237d209d1ad9c2a2e9ca3987ad
Ryobb6 <rryyoobb@gmail.com>
2021-08-25 14:10:25 +0900

    KartGame及びsampleRPGプロジェクト追加
    変更:追加のみ
    特記事項:無し
    0825/14:10 R.Tsuji

---
+++ KartGame/KartGame/Kart.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KartGame
+{
+    class Kart
+    {
+        public int weight;
+        public int speed;
+
+        // アクセルを踏むメソッド
+        public void Force()
+        {
+            Console.WriteLine("加速or減速");
+        }
+
+        // クラクションを鳴らす
+        public virtual void Horn()
+        {
+            Console.WriteLine("ブッブー");
+
+        }
+
+    }
+}
---
+++ SampleRPG/SampleRPG/Player.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SampleRPG
+{
+    class Player
+    {
+
+        private string name;
+        private int hp;
+
+        public Player(string name, int hp)
+        {
+            this.name = name;
+            this.hp = hp;
+        }
+
+        public int Hp
+        {
+            set
+            {
+                this.hp = value;
+                if (this.hp < 0)
+                {
+                    this.hp = 0;
+                }
+            }
+            get
+            {
+                return this.hp;
+            }
+
+        }
+
+        public void Attack()
+        {
+            Console.WriteLine(this.name + "は攻撃した");
+        }
+        public void Defence()
+        {
+            Console.WriteLine(this.name + "は防御した");
+        }
+
+    }
+}
