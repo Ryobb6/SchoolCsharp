3c95dbfe3a1c48245e594550c7821705fdaff9ab
Ryobb6 <78227356+Ryobb6@users.noreply.github.com>
2021-08-30 07:37:14 +0900

    07370830
    WindowsApplication

---
+++ HelloWorld/HelloWorld/HelloWorld/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿
+namespace HelloWorld
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.helloButton = new System.Windows.Forms.Button();
+            this.helloLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // helloButton
+            // 
+            this.helloButton.Location = new System.Drawing.Point(274, 355);
+            this.helloButton.Name = "helloButton";
+            this.helloButton.Size = new System.Drawing.Size(75, 23);
+            this.helloButton.TabIndex = 0;
+            this.helloButton.Text = "クリック!";
+            this.helloButton.UseVisualStyleBackColor = true;
+            this.helloButton.Click += new System.EventHandler(this.HelloButtonClicked);
+            // 
+            // helloLabel
+            // 
+            this.helloLabel.AutoSize = true;
+            this.helloLabel.Location = new System.Drawing.Point(286, 220);
+            this.helloLabel.Name = "helloLabel";
+            this.helloLabel.Size = new System.Drawing.Size(35, 19);
+            this.helloLabel.TabIndex = 1;
+            this.helloLabel.Text = "???";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(648, 534);
+            this.Controls.Add(this.helloLabel);
+            this.Controls.Add(this.helloButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button helloButton;
+        private System.Windows.Forms.Label helloLabel;
+    }
+}
+
