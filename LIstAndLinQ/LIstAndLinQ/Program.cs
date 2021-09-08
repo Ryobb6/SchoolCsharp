b80f046901c410343529cdc4b88afada6815f910
Ryobb6 <rryyoobb@gmail.com>
2021-09-08 15:17:56 +0900

    1520
    1520

---
+++ LIstAndLinQ/LIstAndLinQ/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LIstAndLinQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] hp = { 550, 420, 600, 80, 200 };
+            List<int> newHp = new List<int>();
+
+            var numHp = hp.Select(n => n - 100);
+
+            foreach(int n in numHp)
+            {
+                Console.WriteLine(n);
+            }
+
+        }
+    }
+}
